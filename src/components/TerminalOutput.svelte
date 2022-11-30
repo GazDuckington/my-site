1f8f0784d28db3b6c6b5f47307d8f849e8870ab2
Gaz <dianghazy@protonmail.com>
2022-12-01 03:36:19 +0700

    update node

--- src/components/TerminalOutput.svelte
+++ src/components/TerminalOutput.svelte
@@ -4,7 +4,7 @@
 
 <div class="text-violet-600 dark:text-violet-500 px-6">Type 'help' for more information.</div>
 {#if output}
-	<div class="text-indigo-700 dark:text-indigo-500 px-6">
+	<div class="text-indigo-500 dark:text-indigo-500 px-6">
 		{#each output as op}
 			{@html op}
 		{/each}
