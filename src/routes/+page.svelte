46559a5e11bc60e1dbfe2298d746df9494f78be0
Ghazy <dianghazy@protonmail.com>
2023-07-30 00:38:12 +0700

    oops

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,13 @@
 <script lang="ts">
+	import Terminal from '$comp/Terminal.svelte';
 </script>
 
-
+<div
+	class="
+		translate-all easi-in-out duration-150 
+		flex items-center justify-center h-screen 
+		font-mono
+	"
+>
+	<Terminal />
+</div>
